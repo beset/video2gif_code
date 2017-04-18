9b9876140b014003da5050f910f4ef7c29426d9a
isan <3isan333@gmail.com>
2017-04-18 22:11:59 +0800

    no message

--- generateGifs.py
+++ generateGifs.py
@@ -20,7 +20,7 @@ import sys
 videosDir = sys.argv[1]
 topCount = int(sys.argv[2])
 clipDuration = int(sys.argv[3])
-outputDir = sys.argv[4]
+outputDir = sys.argv[4].decode('utf-8')
 
 
 print outputDir
