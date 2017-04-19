847ac9bce3d2aecf8363e1db8e9912defa451b90
isan <3isan333@gmail.com>
2017-04-19 16:30:05 +0800

    no message

---
+++ whole.py
@@ -0,0 +1,16 @@
+#-*- coding:utf-8 -*-
+from IPython.display import Image, display
+import os
+from moviepy.editor import VideoFileClip
+
+videoPath = './run0414.mp4'
+
+
+video = VideoFileClip(videoPath)
+video_name=os.path.splitext(os.path.split(videoPath)[1])[0].decode('utf-8')
+print video_name
+
+clip = video.subclip(20, 620)
+
+print video.fps
+clip.write_videofile(video_name + "_01.mp4", audio=False)
